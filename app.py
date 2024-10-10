40ca6261380f5ef92ec6d40dbef8600bcf356233
G.Sacha <gueydon.sacha@gmail.com>
2024-10-10 18:59:32 +0200

    produits.html + produits.json

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
+
+def load_data():
+    with open('produits.json') as f:
+        return json.load(f)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/drogues')
+def drogues():
+    data = load_data()
+    category = next((cat for cat in data['categories'] if cat['name'] == 'Drogues'), None)
+    return render_template('produits.html', category=category)
+
+@app.route('/armes')
+def armes():
+    data = load_data()
+    category = next((cat for cat in data['categories'] if cat['name'] == 'Armes'), None)
+    return render_template('produits.html', category=category)
+
+
+@app.route('/papiers')
+def papiers():
+    data = load_data()
+    category = next((cat for cat in data['categories'] if cat['name'] == 'Papiers'), None)
+    return render_template('produits.html', category=category)
+
 
 if __name__ == '__main__':
     app.run()
